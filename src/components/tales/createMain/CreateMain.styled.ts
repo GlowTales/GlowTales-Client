7adab38d78a0ceaf694c094eab569fae970c7eae
CJY <cjy3458@naver.com>
2024-08-19 01:43:37 +0900

    design: 동화 생성 페이지 정렬 방식 수정

--- src/components/tales/createMain/CreateMain.styled.ts
+++ src/components/tales/createMain/CreateMain.styled.ts
@@ -5,7 +5,7 @@ export const Wrapper = styled.div`
   min-height: 100vh;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: space-evenly;
   align-items: center;
   overflow-x: hidden;
   gap: 2.5rem;
@@ -29,6 +29,7 @@ export const Title = styled.div`
 export const SelectContainer = styled.div`
   width: 100%;
   height: 100%;
+  max-height: 400px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
